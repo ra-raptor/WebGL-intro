5c161a8f71bbba72f10ccd0dfd0eca817022f47f
Vikas Kumar Saw <vikasvks0112@gmail.com>
2021-05-14 17:21:02 +0530

    colors

--- main.js
+++ main.js
@@ -5,18 +5,28 @@ if (!gl) {
   throw new Error("WebGL is not supported");
 }
 
+const colorData = [1, 0, 0, 0, 1, 0, 0, 0, 1];
+
 const vertexData = [0, 1, 0, 1, -1, 0, -1, -1, 0];
-const buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
 
 //vertex shader
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(
   vertexShader,
   `
+precision mediump float;
 attribute vec3 position;
+attribute vec3 color;
+varying vec3 vColor;
 void main(){
+    vColor = color;
     gl_Position = vec4(position,1);
 }
 `
@@ -28,8 +38,10 @@ const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(
   fragmentShader,
   `
+  precision mediump float;
+varying vec3 vColor;
 void main(){
-    gl_FragColor = vec4(1,0,0,1);
+    gl_FragColor = vec4(vColor,1);
 }
 `
 );
@@ -44,7 +56,13 @@ gl.linkProgram(program);
 //enable vertex attribute
 const positionLocation = gl.getAttribLocation(program, `position`);
 gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+//enable vertex attribute
+const colorLocation = gl.getAttribLocation(program, `color`);
+gl.enableVertexAttribArray(colorLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
 gl.useProgram(program);
 gl.drawArrays(gl.TRIANGLES, 0, 3);
